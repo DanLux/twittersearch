cd28ab6cdd41dcceb224efb5c8e3beae4c7406d6
Daniel Alves <danpaulalves@gmail.com>
2017-10-30 02:46:02 -0200

    Added javascript code to handle search requests.

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,31 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
+
+function search() {
+	let query = $.trim($("#search-query").val());
+    if (query) {
+    	$.ajax({
+    		type:'GET',
+    		url: "/tweets/" + encodeURIComponent(query),
+    		success: function() {}
+    	});
+    }
+}
+
+function add_enter_key_listener() {
+	$("#search-query").keypress(function(event) {
+		let key = event.which || event.keyCode;
+	    if (key === 13) {
+	        event.preventDefault();
+	        search()
+	    }
+	})
+}
+
+function add_search_button_listener() {
+	$("#search-button").click(search);
+}
+
+
+$(document).ready(function() {
+ 	add_enter_key_listener();
+ 	add_search_button_listener();
+});
